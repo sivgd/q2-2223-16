c913e765369d1a62359bba8bec5f3ffa4fbf5127
MUKILTEO\1713574 <1713574@mukilteo.wednet.edu>
2022-12-09 09:34:32 -0800

    started player movement

---
+++ Q2Project_Team16/Assets/Alex/Movement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    bool grounded = false;
+    Rigidbody2D rb2;
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2 = gameObject.GetComponent<Rigidbody2D>();
+        sr = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizValue = Input.GetAxis("Horizontal");
+
+        rb2.velocity = new Vector2(horizValue * 4, rb2.velocity.y);
+
+        if (horizValue > 0)
+        {
+            sr.flipX = false;
+        }
+        if (horizValue < 0)
+        {
+            sr.flipX = true;
+        }
+
+        grounded = Physics2D.BoxCast(transform.position, new Vector2(0.1f, 0.1f), 0, Vector2.down, 1, LayerMask.GetMask("Ground"));
+        if (grounded && Input.GetKeyDown(KeyCode.W) || grounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2.velocity = new Vector2(rb2.velocity.x, 6);
+        }
+    }
+}
---
+++ Q2Project_Team16/Assets/Alex/SuperJump.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperJump : MonoBehaviour
+{
+   //nothing yet lol...
+}
