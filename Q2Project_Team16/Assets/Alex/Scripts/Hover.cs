92c27c609dbd4cadfe44bee4cb92854cfd0d5850
MUKILTEO\1713574 <1713574@mukilteo.wednet.edu>
2022-12-13 10:23:50 -0800

    started jump mechanic

---
+++ Q2Project_Team16/Assets/Alex/Scripts/Hover.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hover : MonoBehaviour
+{
+    public float speed = 5f;
+    public float height = 0.5f;
+
+    Vector3 pos;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+    void Update()
+    {
+        float newY = Mathf.Sin(Time.time * speed) * height + pos.y;
+        transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+    }
+
+}
