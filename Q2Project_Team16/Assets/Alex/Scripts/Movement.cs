4eb5a7ad19c48b9d1f1ae31b2aa608d1a51cacab
MUKILTEO\1713574 <1713574@mukilteo.wednet.edu>
2022-12-13 09:52:43 -0800

    player movement done

---
+++ Q2Project_Team16/Assets/Alex/Scripts/Movement.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpForce;
+    public SpriteRenderer sr;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(moveInput, 0, 0) * moveSpeed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space)&& Mathf.Abs(rb.velocity.y)<0.001f || Input.GetKeyDown(KeyCode.W) && Mathf.Abs(rb.velocity.y) < 0.001f)
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            sr.flipX = false;
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sr.flipX = true;
+        }
+    }
+}
