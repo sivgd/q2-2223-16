df4ed3ca4b70f9234cd249b27de79b28e0db5261
MUKILTEO\1713574 <1713574@mukilteo.wednet.edu>
2023-01-03 10:22:40 -0800

    crocodile added

---
+++ Q2Project_Team16/Assets/Alex/Scripts/CrocPatrol.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrocPatrol : MonoBehaviour
+{
+    public float speed;
+    public float distance;
+    public Transform groundCheck;
+    bool movingRight = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundCheck.position, Vector2.down, distance);
+        if(groundInfo.collider == false)
+        {
+            if (movingRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                movingRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                movingRight = true;
+            }
+        }
+    }
+    
+}
