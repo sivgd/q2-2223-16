a3a04f5a9a584624dc0e9f7150ea535af236975d
MUKILTEO\1713574 <1713574@mukilteo.wednet.edu>
2023-01-12 10:24:39 -0800

    death  animation work

---
+++ Q2Project_Team16/Assets/Alex/Scripts/Transitions.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Transitions : MonoBehaviour
+{
+    public Animator transition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transition.SetBool("In", true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
