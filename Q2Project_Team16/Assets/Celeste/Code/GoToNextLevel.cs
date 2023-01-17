850ccd41b54f4154663d4ed1e5c30c5fd457165f
CopyCatCopy <copycat4848@gmail.com>
2023-01-17 09:17:47 -0800

    Added a "triger" to an animation

--- Q2Project_Team16/Assets/Celeste/Code/GoToNextLevel.cs
+++ Q2Project_Team16/Assets/Celeste/Code/GoToNextLevel.cs
@@ -1,30 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class PlayPaw : MonoBehaviour
+public class GoToNextLevel : MonoBehaviour
 {
-    public GameObject obj;
+    public string Scene;
 
     // Start is called before the first frame update
     void Start()
     {
-        obj.SetActive(false);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.tag == "Player")
+        if(collision.tag == "Player")
         {
-            obj.SetActive(true);
+            Debug.Log("Switch scene");
+            SceneManager.LoadScene("AlexScene");
         }
     }
-
-    private void OnTriggerExit2D(Collider2D collision) => obj.SetActive(false);
 }
